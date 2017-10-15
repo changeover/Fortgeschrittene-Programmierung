8b32752af965f74cc11435f798f29edf599a33cf
changeover <leaotspam@gmail.com>
2017-10-15 19:16:50 +0200

    New

---
+++ HeapTest.java
@@ -0,0 +1,12 @@
+public class HeapTest {
+    public static void main(String[] args) {
+        Heap <float> heap[] = new Heap<>();
+        public void populate()
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                heap[i] = (100.0*Math.random());
+            }
+        }
+    }
+}
